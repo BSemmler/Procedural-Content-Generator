14441767694dc1f72a56ec7719d4cc24d1df6033
Brett Semmler <brett.semmler@hotmail.com>
2024-03-02 15:41:03 -0700

    app is now compilable again.

--- KVGEngine/RenderDeviceDX11.cpp
+++ KVGEngine/RenderDeviceDX11.cpp
@@ -1,13 +1,11 @@
 #include "RenderDeviceDX11.h"
 
-using namespace KGV::Util;
-
 namespace KGV::Render {
     bool RenderDeviceDX11::init() {
         return false;
     }
 
-    Texture2dDX11 *RenderDeviceDX11::createTexture2D(Texture2dConfigDX11 &config, ResourceData &data, ResourceType type) {
+    Texture2dDX11 * RenderDeviceDX11::createTexture2D(Texture2dConfigDX11 &config, ResourceData &data, eResourceType type) {
         auto *texture = new Texture2dDX11();
         HRESULT hr = device->CreateTexture2D(&config.desc, reinterpret_cast<D3D11_SUBRESOURCE_DATA *>(&data), &texture->texture);
 
@@ -29,23 +27,153 @@ namespace KGV::Render {
         return texture;
     }
 
-    BufferDX11 *RenderDeviceDX11::createBuffer(BufferConfigDX11 &config, ResourceData &data, ResourceType type) {
-        auto *buff = new BufferDX11();
-        HRESULT hr = device->CreateBuffer(&config.desc, reinterpret_cast<D3D11_SUBRESOURCE_DATA *>(&data), &buff->buffer);
+//    BufferDX11 * RenderDeviceDX11::createBuffer(BufferConfigDX11 &config, ResourceData &data, eResourceType type) {
+//        auto *buff = new BufferDX11();
+//        HRESULT hr = device->CreateBuffer(&config.desc, reinterpret_cast<D3D11_SUBRESOURCE_DATA *>(&data), &buff->buffer);
+//
+//        if (FAILED(hr)) {
+//            logger.error("Failed to create D3D11 Buffer!");
+//            // Trigger a breakpoint if we're in a debug build.
+//            KGV_debugBreak();
+//            delete buff;
+//            buff = nullptr;
+//        }
+//
+//        if (buff) {
+//            buff->actualDesc = config.desc;
+//            buff->desiredDesc = buff->actualDesc;
+//        }
+//
+//        return buff;
+//    }
 
-        if (FAILED(hr)) {
-            logger.error("Failed to create D3D11 Buffer!");
-            // Trigger a breakpoint if we're in a debug build.
-            KGV_debugBreak();
-            delete buff;
-            buff = nullptr;
+    S32 RenderDeviceDX11::createSwapChain(void* hwnd, SwapChainConfigDX11& config) {
+        ComPtr<IDXGIDevice> dxgiDevice;
+        if (FAILED(device.CopyTo(dxgiDevice.GetAddressOf()))) {
+            logger.error("Failed to acquire DXGIDevice, reason: {}", GetLastError());
+            return -1;
         }
 
-        if (buff) {
-            buff->actualDesc = config.desc;
-            buff->desiredDesc = buff->actualDesc;
+        ComPtr<IDXGIFactory2> dxgiFactory2;
+        if (FAILED(CreateDXGIFactory2(
+                0, __uuidof(IDXGIFactory2), reinterpret_cast<void **>(dxgiFactory2.GetAddressOf())))) {
+            logger.error("Failed to acquire DXGIFactory2 interface, reason: {}", GetLastError());
         }
 
-        return buff;
+        HRESULT hr;
+        ComPtr<IDXGISwapChain1> swapChain;
+        if (FAILED(hr = dxgiFactory2->CreateSwapChainForHwnd(device.Get(), static_cast<HWND>(hwnd), &config.getDesc(), nullptr, nullptr, swapChain.GetAddressOf()))) {
+            logger.error("Failed to create DXGI swap chain, reason: {}", GetLastError());
+            MessageBox(nullptr, "Failed to create swap chain!", "Error!", MB_OK);
+            return false;
+        }
+
+        config.setWidth(config.getDesc().Width);
+        config.setHeight(config.getDesc().Height);
+
+//        D3D11_VIEWPORT viewport;
+//        memset(&viewport, 0, sizeof(D3D11_VIEWPORT));
+//        viewport.TopLeftX = 0.0f;                             // left hand bounds of the viewport.
+//        viewport.TopLeftY = 0.0f;                             // top bounds of the view port
+//        viewport.Width = static_cast< float >( gWidth );   // width of the viewport
+//        viewport.Height = static_cast< float >( gHeight );  // height of the view port
+//
+//        // Bind the viewport.
+//        gpImmediateContext->RSSetViewports(1, &viewport);
+
+        // Get the surface of the backbuffer and then create a render target view for that surface.
+        ComPtr<ID3D11Texture2D> backBuffer;
+        swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void **>(backBuffer.GetAddressOf()));
+
+//        // Get the resource view for our back buffer.
+//        if (FAILED(hr = device->CreateRenderTargetView(gpBackBuffer.Get(), nullptr, &gpBackBufferTarget))) {
+//            OutputDebugStringA("Error: Failed to create create render target! Line: " + __LINE__);
+//            MessageBox(0, L"Failed to create render target!", L"Error!", MB_OK);
+//            return false;
+//        }
+
+        return true;
+    }
+
+    S32 RenderDeviceDX11::createShaderResourceView(S32 resourceId, D3D11_SHADER_RESOURCE_VIEW_DESC *desc) {
+        auto resource = getResourceById(resourceId);
+
+        bool isResourceIdValid = false;
+        bool isResourceInitialized = false;
+        HRESULT hr = 0;
+
+        if (resource) {
+            isResourceIdValid = true;
+            if (resource->getResource()) {
+                isResourceInitialized = true;
+                ComPtr<ID3D11ShaderResourceView> view;
+
+                hr = device->CreateShaderResourceView(resource->getResource(), desc, &view);
+                if (view) {
+                    ShaderResourceViewDX11 srv(view);
+                    shaderResourceViews.push_back(srv);
+                    auto srvId = shaderResourceViews.size() - 1;
+                    logger.trace("Successfully created Shader Resource View {} for resource {}", srvId, resourceId);
+                    return static_cast<S32>(srvId);
+                }
+            }
+
+        }
+
+        logger.debug("Failed to create Shader Resource View, resource {}, valid resource: {}, resource initialized: {}, HRESULT: {}",
+                     resourceId, isResourceIdValid, isResourceInitialized, hr);
+
+        return -1;
+    }
+
+    S32 RenderDeviceDX11::createRenderTargetView(S32 resourceId, D3D11_RENDER_TARGET_VIEW_DESC *desc) {
+        auto resource = getResourceById(resourceId);
+
+        bool isResourceIdValid = false;
+        bool isResourceInitialized = false;
+        HRESULT hr = 0;
+
+        if (resource) {
+            isResourceIdValid = true;
+            if (resource->getResource()) {
+                isResourceInitialized = true;
+                ComPtr<ID3D11RenderTargetView> view;
+
+                hr = device->CreateRenderTargetView(resource->getResource(), desc, &view);
+                if (view) {
+                    RenderTargetViewDX11 rtv(view);
+                    renderTargetViews.push_back(rtv);
+                    auto rtvId = renderTargetViews.size() - 1;
+                    logger.trace("Successfully created Render Target View {} for resource {}", rtvId, resourceId);
+                    return static_cast<S32>(rtvId);
+                }
+            }
+
+        }
+
+        logger.debug("Failed to create Render Target View, resource {}, valid resource: {}, resource initialized: {}, HRESULT: {}",
+                     resourceId, isResourceIdValid, isResourceInitialized, hr);
+
+        return -1;
+    }
+
+    ResourceDX11* RenderDeviceDX11::getResourceById(S32 index) {
+        if (index < 0 || index >= resources.size())
+            return nullptr;
+
+        return resources[index].get();
+    }
+
+    bool RenderDeviceDX11::deleteResource(const std::shared_ptr<ResourceViewDX11>& resource) {
+        return this->deleteResource(resource->getResourceId());
+    }
+
+    bool RenderDeviceDX11::deleteResource(int index) {
+        if (index < 0 || index >= resources.size())
+            return false;
+
+        this->resources[index].reset(nullptr);
+
+        return true;
     }
 }
--- KVGEngine/RenderDeviceDX11.h
+++ KVGEngine/RenderDeviceDX11.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef RENDER_DEVICE_DX11_H_
+#define RENDER_DEVICE_DX11_H_
 
 #include "pch.h"
 #include "Texture2dDX11.h"
@@ -6,20 +7,15 @@
 #include "BufferConfigDX11.h"
 #include "Texture2dConfigDX11.h"
 #include "ResourceData.h"
-
-enum class ResourceType : U32 {
-    kVertexBuffer       = 0x01,
-    kIndexBuffer        = 0x02,
-    kConstantBuffer     = 0x03,
-    kStructuredBuffer   = 0x04,
-    kByteAddressBuffer  = 0x05,
-    kReservedForFuture  = 0x06,
-    kTexture1D          = 0x07,
-    kTexture2D          = 0x08,
-    kTexture3D          = 0x09
-};
+#include "SwapChainConfigDX11.h"
+#include "ShaderResourceViewDX11.h"
+#include "RenderTargetViewDX11.h"
+#include "ResourceDX11.h"
+#include "ResourceViewDX11.h"
 
 namespace KGV::Render {
+    class ResourceViewDX11;
+
     class RenderDeviceDX11 {
     public:
         RenderDeviceDX11();
@@ -31,25 +27,36 @@ namespace KGV::Render {
         void shutdown();
 
         //bool createTexture1D();
-        Texture2dDX11 *createTexture2D(Texture2dConfigDX11 &config, ResourceData &data, ResourceType type);
+        Texture2dDX11 * createTexture2D(Texture2dConfigDX11 &config, ResourceData &data, eResourceType type);
         //bool createTexture3D();
 
-        BufferDX11 *createBuffer(BufferConfigDX11 &config, ResourceData &data, ResourceType type);
+//        BufferDX11 * createBuffer(BufferConfigDX11 &config, ResourceData &data, eResourceType type);
 
-        S32 createSwapchain();
+        S32 createSwapChain(void* windowHandle, SwapChainConfigDX11& config);
 
-        void createShaderResourceView();
+        S32 createShaderResourceView(S32 resourceId, D3D11_SHADER_RESOURCE_VIEW_DESC* desc);
 
-        void createRenderTargetView();
+        S32 createRenderTargetView(S32 resourceId, D3D11_RENDER_TARGET_VIEW_DESC* desc);
+
+        ResourceDX11* getResourceById(S32 index);
+
+        bool deleteResource(const std::shared_ptr<ResourceViewDX11>& resource);
+        bool deleteResource(S32 index);
 
     protected:
         ComPtr<ID3D11Device1> device;
-        //std::vector<ComPtr<IDXGISwapChain1>> swapchains;
+        std::vector<ComPtr<IDXGISwapChain1>> swapChains;
         std::vector<BufferDX11> buffers;
         std::vector<Texture2dDX11> textures;
+        std::vector<ShaderResourceViewDX11> shaderResourceViews;
+        std::vector<RenderTargetViewDX11> renderTargetViews;
+        std::vector<std::unique_ptr<ResourceDX11>> resources;
+        std::vector<S32> availableIds;
         spdlog::logger logger;
 
     private:
 
     };
 }
+
+#endif // RENDER_DEVICE_DX11_H_
